25d0d94048c1515a5ae9750884f9a7039ab37833
David Miotti <david@muxumuxu.com>
2017-04-14 18:49:16 +0200

    Added worker for metex

---
+++ apps/metex/lib/metex.ex
@@ -0,0 +1,18 @@
+defmodule TheLittle.Metex do
+  @moduledoc """
+  Documentation for TheLittle.Metex.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> TheLittle.Metex.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ apps/metex/test/metex_test.exs
@@ -0,0 +1,8 @@
+defmodule TheLittle.MetexTest do
+  use ExUnit.Case
+  doctest TheLittle.Metex
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
